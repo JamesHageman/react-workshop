229aae01cc0d55aa89bbb406db02f2c5dac71a95
James Hageman <jamesd@hageman.ca>
2016-04-30 11:15:24 -0400

    Add support for redux-devtools extension

--- todo-app-redux-immutable/app/store/configure-store.js
+++ todo-app-redux-immutable/app/store/configure-store.js
@@ -1,6 +1,10 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/index.js';
 
-export default function configureStore() {
-  return createStore(rootReducer);
+export default function configureStore(initialState) {
+  const middleware = [];
+  return createStore(rootReducer, initialState, compose(
+    applyMiddleware(...middleware),
+    window && window.devToolsExtension ? window.devToolsExtension() : f => f
+  ));
 }
