0f59ef8b9a743e6020411e9aab2dd9908cd4c64e
James Hageman <jamesd@hageman.ca>
2016-05-01 12:53:12 -0400

    Add todo-form tests

--- todo-app-redux-immutable/app/components/todo/todo-form/todo-form.test.js
+++ todo-app-redux-immutable/app/components/todo/todo-form/todo-form.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import TestUtils from 'react/lib/ReactTestUtils';
 import expect, { createSpy } from 'expect';
 import TodoForm from "./todo-form";
@@ -6,14 +7,33 @@ import TodoForm from "./todo-form";
 describe('todo-form', () => {
   var _component
   var _handleSave
+  var node
 
   beforeEach(() => {
     _handleSave = createSpy();
     _component = TestUtils.renderIntoDocument( <TodoForm onSave={_handleSave} /> );
-  })
+    ReactDOM.render(<TodoForm onSave={_handleSave} />, document.body, function() {
+      node = ReactDOM.findDOMNode(this);
+    });
+  });
+
   it('renders without problems', () => {
     expect(_component).toExist();
-  })
-  it('TODO: I will write some tests');
+  });
+
+  it('calls save with the entered text and clears the text', () => {
+    var input = node.querySelector('input');
+
+    input.value = 'Write some tests';
+
+    TestUtils.Simulate.change(input);
+    TestUtils.Simulate.keyDown(input, {key: "Enter", keyCode: 13, which: 13});
+
+    expect(_handleSave.calls.length).toEqual(1);
+    expect(_handleSave.calls[0].arguments[0]).toEqual('Write some tests');
+
+    expect(input.value).toEqual('');
+
+  });
 });
 
